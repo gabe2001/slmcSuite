9b9cd65d4b49be210ecb5c4111571e8f896882e4
Gabriel Inäbnit <gabe.inabnit@gmail.com>
2013-09-11 19:58:35 +0200

    home

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ www.slotracinglemans.com App Suite
 =
 
 To do:
-* back button for devices without a physical button
 * a ton of work still ahead
 
 Issues:
-* app on Android not loading when using slm.c image
+* 
--- source/Home.js
+++ source/Home.js
@@ -3,23 +3,36 @@ enyo.kind({
 	kind: "enyo.FittableRows",
 	classes: "enyo-fit enyo-unselectable",
 	components: [
-		{content: "<b>Main Panel</b> - hot news, main article, etc", allowHtml: true},
-		{content: "To do: query slm.c for article content based on relevance and/or news factor, then do some formatting suited for mobile devices",
-			style: "color: grey;"},
-		{kind: "enyo.Panels", fit: true, dragable: false, components: [
+		{name: "title", allowHtml: true, style: "font-weight: bold;"},
+		{kind: "onyx.Drawer", name: "introDrawer", open: true, components: [
+				{name: "intro", allowHtml: true, style: "font-weight: normal; font-style: italic;"}
+			]},
+		{kind: "enyo.Panels", fit: true, dragable: false, ontap: "toggleIntro", components: [
 				{kind: "enyo.Scroller", touch: true, components: [
-						{content: "<img src='http://www.slotracinglemans.com/images/pict/Hotslot_newtrack.gif'>",
-							allowHtml: true},
-						{content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit,<br>sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna<br>aliquam erat volutpat.<br>Ut wisi enim ad minim veniam,<br>quis nostrud exerci tation ullamcorper suscipit<br>lobortis nisl ut aliquip ex ea commodo consequat.<br>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,<br>vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.<br>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.<br>Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.<br>Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius.<br>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.<br>Mirum est notare quam littera gothica,<br>quam nunc putamus parum claram,<br>anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima.<br>Eodem modo typi,<br>qui nunc nobis videntur parum clari,<br>fiant sollemnes in futurum.<br>",
-							allowHtml: true},
-						{content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit,<br>sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna<br>aliquam erat volutpat.<br>Ut wisi enim ad minim veniam,<br>quis nostrud exerci tation ullamcorper suscipit<br>lobortis nisl ut aliquip ex ea commodo consequat.<br>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,<br>vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.<br>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.<br>Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.<br>Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius.<br>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.<br>Mirum est notare quam littera gothica,<br>quam nunc putamus parum claram,<br>anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima.<br>Eodem modo typi,<br>qui nunc nobis videntur parum clari,<br>fiant sollemnes in futurum.<br>",
-							allowHtml: true,
-							style: "color: #0000CC"}
+						{name: "article", allowHtml: true}
 					]}
 			]}
 	],
 	create: function() {
 		this.inherited(arguments);
-		// initialization code goes here
+		this.loadMainArticle();
+	},
+	toggleIntro: function() {
+		this.$.introDrawer.setOpen(!this.$.introDrawer.open);
+	},
+	loadMainArticle: function() {
+		var req = new enyo.JsonpRequest({
+			url: "http://www.slotracinglemans.com/newforum/slmc.php"
+		});
+		req.go({action: "mainarticle"});
+		req.response(this, "loadContent");
+	},
+	loadContent: function(inSender, inEvent) {
+		enyo.log("loadContent", inEvent);
+		if (inEvent && inEvent.length > 0) {
+			this.$.title.setContent(inEvent[0].title);
+			this.$.intro.setContent(inEvent[0].intro.replace('\\', ''));
+			this.$.article.setContent(inEvent[0].content.replace('\\', ''));
+		}
 	}
 });
