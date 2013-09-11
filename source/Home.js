d10942a014318165859397a4b0d725f51873be3c
Gabriel Inäbnit <gabe.inabnit@gmail.com>
2013-09-12 00:51:51 +0200

    scrolling & splash

--- source/Home.js
+++ source/Home.js
@@ -4,22 +4,28 @@ enyo.kind({
 	classes: "enyo-fit enyo-unselectable",
 	components: [
 		{name: "title", allowHtml: true, style: "font-weight: bold;"},
-		{kind: "onyx.Drawer", name: "introDrawer", open: true, components: [
-				{name: "intro", allowHtml: true, style: "font-weight: normal; font-style: italic;"}
-			]},
 		{kind: "enyo.Panels", fit: true, dragable: false, ontap: "toggleIntro", components: [
 				{kind: "enyo.Scroller", touch: true, components: [
+						{name: "intro", allowHtml: true, style: "font-weight: normal; font-style: italic;"},
 						{name: "article", allowHtml: true}
-					]}
+				]}
 			]}
+		// {kind: "onyx.Drawer", name: "introDrawer", open: true, ontap: "toggleIntro", components: [
+		// 		{name: "intro", allowHtml: true, style: "font-weight: normal; font-style: italic;"}
+		// 	]},
+		// {kind: "enyo.Panels", fit: true, dragable: false, ontap: "toggleIntro", components: [
+		// 		{kind: "enyo.Scroller", touch: true, components: [
+		// 				{name: "article", allowHtml: true}
+		// 			]}
+		// 	]}
 	],
 	create: function() {
 		this.inherited(arguments);
 		this.loadMainArticle();
 	},
-	toggleIntro: function() {
-		this.$.introDrawer.setOpen(!this.$.introDrawer.open);
-	},
+	// toggleIntro: function() {
+	// 	this.$.introDrawer.setOpen(!this.$.introDrawer.open);
+	// },
 	loadMainArticle: function() {
 		var req = new enyo.JsonpRequest({
 			url: "http://www.slotracinglemans.com/newforum/slmc.php"
